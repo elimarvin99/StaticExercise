69d45e3d5a951e5b81ad6231fac88412cfff3fa4
elimarvin99 <elimarvin99@gmail.com>
2021-11-08 15:55:16 -0500

    completed exercise

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter degree to be converted from Celcius to Fahrenheit:");
+            var input = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter degree to be converted from Fahrenheit to Celcius:");
+            var input1 = Convert.ToDouble(Console.ReadLine());
+
+            
+            var fahrenheit = TempConverter.CelciusToFahrenheit(input);
+            var celcius = TempConverter.FahrenheitToCelcius(input1);
+
+            Console.WriteLine("After conversion:");
+            Console.WriteLine($"{input} degrees Celcius is {fahrenheit} degrees in Fahrenheit.");
+            Console.WriteLine($"{input1} degrees Fahrenheit is {celcius} degrees in Celcius.");
+            
+        }
+    }
+}
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelcius (double a)
+        {
+            return (a - 32) * 5/9;
+        }
+        public static double CelciusToFahrenheit (double a)
+        {
+            return (a * 9 / 5) + 32;
+        }
+            
+    }
+}
